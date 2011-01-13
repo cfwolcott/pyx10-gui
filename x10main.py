110c6304a5e94dc53fd59bc580e2cf0ae553352d
cwolcott <cwolcott@fad39a76-5cd8-b11b-88a8-6537d15b3518>
2011-01-13 20:31:52 +0000

    Initial Check-in

---
+++ x10main.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+
+from PyQt4 import QtCore, QtGui
+from ui.MainWindow import MainWindow
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    ui = MainWindow()
+    ui.show()
+    sys.exit(app.exec_())
